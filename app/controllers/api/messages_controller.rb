eb39c78179b2e0835757da14f709faf307b350d8
aitiiwase <bicky_long_time_ago@yahoo.co.jp>
2019-09-20 10:30:15 +0900

    edit-auto

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,9 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
-    respond_to do |format|
-      format.html
-      format.json{@messages = @group.messages.where('id > ?', params[:id])}  #ajaxで渡されたlast_message_idより大きい値のidを持つmessageを取得
-    end
+    @messages = @group.messages.includes(:user).where('id > ?', params[:id])
   end
 end
